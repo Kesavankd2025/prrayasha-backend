ba6e623962e96592e9f16e3e362405ac94ad53ef
Kesavan0611 <kesavan@oceansoftteam.in>
2026-04-29 18:52:48 +0530

    dd

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,13 +8,14 @@ dotenv.config({ quiet: true });
 
 const isProd = process.env.NODE_ENV === "prod";
 
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/Prrayasha-collection-Dev";
 const DB_URL = "mongodb://localhost:27017";
 const DB_NAME = "Prrayasha-collection-Dev";
 
 
 export const AppDataSource = new DataSource({
     type: "mongodb",
-    url: DB_URL + "/" + DB_NAME || "",
+    url: MONGO_URI || "",
     synchronize: false,
     logging: !isProd,
 
